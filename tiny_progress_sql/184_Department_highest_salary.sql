84555466c3752d75fe17f600f5cd614499d150ef
sahilthenagpal <thisissahilnagpal@gmail.com>
2022-06-13 20:49:17 +0530

    leet

---
+++ tiny_progress_sql/184_Department_highest_salary.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+-- Working: 
+
+# select b.name as Department ,a.name as Employee , a.salary as Salary
+# from employee a left join department b on a.departmentid = b.id 
+# where departmentId is not null
+# group by salary having(b.salary) = max(b.salary) 
+ 
+# ;
+
