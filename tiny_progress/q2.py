0402f19072614be7b047ac8f4f71369398f7e758
SagarikaNagpal <nagpalsagarika@gmail.com>
2023-01-03 03:05:39 -0500

    w3Resources

---
+++ tiny_progress/q2.py
@@ -0,0 +1,10 @@
+"Write a Python program to get the Python version you are using."
+import sys,
+print(sys.version)
+# Write a Python program to display the current date and time.
+# Sample Output :
+# Current date and time :
+# 2014-07-05 14:34:14
+print("\n")
+d1 = datetime.now()
+print(d1)
